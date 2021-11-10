8f0e34e9e2ac18a3f66e5e986e80070b0d7f4172
Rheitheillaethor <minegocu3@gmail.com>
2021-11-10 17:39:15 +0100

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int GettingInfo()
+        {
+            Console.WriteLine("Give me last integer number, you want to check in FizzBuzz");
+            string givenAnswer = Console.ReadLine();
+        }
+
+    }
+}
