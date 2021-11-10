c435ee59da5811d175ad6b08ea4d07bd5fcfd8cd
Rheitheillaethor <minegocu3@gmail.com>
2021-11-10 20:21:17 +0100

    I have created two methods responsible for taking data - answer from player and generate computer answer, third method is going to compare data and then method will decide who the winner is.

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,12 +6,42 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //GettingInfoFirstPlayer
+            Console.WriteLine(GettingInfoFromPlayer());
         }
-        static int GettingInfo()
+        static string GettingInfoFromPlayer()
         {
-            Console.WriteLine("Give me last integer number, you want to check in FizzBuzz");
-            string givenAnswer = Console.ReadLine();
+            string[] fighters = new string[] { "rock", "paper", "scissors" };
+            Console.WriteLine("Choose your figther");
+            Console.WriteLine("type: 'rock', 'paper' or 'scissors'");
+            string firstPlayerChoice = Console.ReadLine();
+            string check = Array.Find(fighters, element => element == firstPlayerChoice);
+            if (check == firstPlayerChoice)
+            {
+                return firstPlayerChoice;
+            }
+            else
+            {
+                Console.WriteLine("It is wrong fighter! \nTry again!");
+                return GettingInfoFromPlayer();
+            }
+        }
+        static string RandomComputerAnswer()
+        {
+            Random rnd = new Random();
+            int RandomNumber = rnd.Next(3);
+            switch (RandomNumber)
+            {
+                case 0:
+                    return "rock";
+                    break;
+                case 1:
+                    return "paper";
+                    break;
+                case 2:
+                    return "scissors";
+                    break;
+            }
         }
 
     }
