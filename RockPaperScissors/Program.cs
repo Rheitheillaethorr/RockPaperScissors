321204fd3356f619bde6d48e1f309fb16cf9f778
Rheitheillaethor <minegocu3@gmail.com>
2021-11-11 13:22:26 +0100

    I have added new methods. WhoWIllWin is checking conditions of winning and giving answer. TryAgain just allows player to play again without restarting game

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,9 +6,7 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            //GettingInfoFirstPlayer
-            Console.WriteLine(GettingInfoFromPlayer());
-            Console.WriteLine(RandomComputerAnswer());
+            WhoWillWin(GettingInfoFromPlayer(), RandomComputerAnswer());
         }
         static string GettingInfoFromPlayer()
         {
@@ -34,10 +32,13 @@ namespace RockPaperScissors
             switch (RandomNumber)
             {
                 case 0:
+                    Console.WriteLine("Computer choose rock");
                     return "rock";
                 case 1:
+                    Console.WriteLine("Computer choose paper");
                     return "paper";
                 case 2:
+                    Console.WriteLine("Computer choose scissors");
                     return "scissors";
                 default:
                     return RandomComputerAnswer();
@@ -47,10 +48,32 @@ namespace RockPaperScissors
         {
             if (playerAnswer == computerAnswer)
             {
-                Console.WriteLine("DRAW!");
+                Console.WriteLine("Tie! Next round");
+                WhoWillWin(GettingInfoFromPlayer(), RandomComputerAnswer());
             }
-            
+            else if (playerAnswer == "rock" && computerAnswer == "scissors" || playerAnswer == "scissors" && computerAnswer == "paper" || playerAnswer == "paper" && computerAnswer == "rock")
+            {
+                Console.WriteLine("You won!");
+            }
+            else
+            {
+                Console.WriteLine("You lost :(");
+            }
+            TryAgain();
         }
-
+        static void TryAgain()
+        {
+            Console.WriteLine("Do you want to try again?\nType 'yes' for retry or anything else to quit program");
+            string retryAnswer = Console.ReadLine();
+            switch (retryAnswer)
+            {
+                case "yes":
+                    WhoWillWin(GettingInfoFromPlayer(), RandomComputerAnswer());
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
     }
 }
